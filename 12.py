751f3b563eefe49232160563d6ef328cfb362ec6
phb9111 <74388702+phb9111@users.noreply.github.com>
2026-03-05 16:30:57 +0900

    Update 12.py

--- 12.py
+++ 12.py
@@ -6,7 +6,6 @@ from datetime import datetime
 NOTION_TOKEN = os.environ.get('NOTION_TOKEN')
 DATABASE_ID = os.environ.get('NOTION_DATABASE_ID')
 
-# 최상위 경로에 저장 (본사 로비)
 SAVE_PATH = "./" 
 if not os.path.exists(SAVE_PATH):
     os.makedirs(SAVE_PATH)
@@ -38,38 +37,38 @@ def sync_notion_to_blog():
 
     post_links_html = ""
 
-    # 메뉴바
-    nav_bar_html = '''
+    # 🚩 핵심 패치: 모든 링크에 매니저님의 공식 도로명 주소(/startbootstrap-clean-blog/)를 박아 넣습니다.
+    BASE_URL = "/startbootstrap-clean-blog"
+
+    nav_bar_html = f'''
     <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="index.html">퀀트 블로그</a>
+            <a class="navbar-brand" href="{BASE_URL}/index.html">Hyungbin's LAB</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fas fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{BASE_URL}/index.html">Home</a></li>
                 </ul>
             </div>
         </div>
     </nav>
     '''
 
-    # 🚩 핵심 수정: CSS 옷을 'dist/' 창고에서 가져오도록 지정!
-    head_html = '''
+    head_html = f'''
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
-    <link href="dist/css/styles.css" rel="stylesheet" />
+    <link href="{BASE_URL}/dist/css/styles.css" rel="stylesheet" />
     '''
     
-    # 🚩 자바스크립트 스크립트도 'dist/' 창고에서 가져오도록 지정!
-    footer_html = '''
+    footer_html = f'''
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="dist/js/scripts.js"></script>
+    <script src="{BASE_URL}/dist/js/scripts.js"></script>
     '''
 
     for post in posts:
@@ -88,7 +87,7 @@ def sync_notion_to_blog():
         
         post_links_html += f'''
         <div class="post-preview">
-            <a href="{file_name}">
+            <a href="{BASE_URL}/{file_name}">
                 <h2 class="post-title">{title}</h2>
                 <h3 class="post-subtitle">{category}</h3>
             </a>
@@ -113,7 +112,6 @@ def sync_notion_to_blog():
                 if b64_img:
                     body_html += f'<img src="{b64_img}" style="max-width: 100%; height: auto; border-radius: 5px; margin: 30px 0;">\n'
 
-        # 🚩 배경 이미지도 'dist/' 에서 가져오기
         post_page = f'''
         <!DOCTYPE html>
         <html lang="ko">
@@ -123,7 +121,7 @@ def sync_notion_to_blog():
         </head>
         <body>
             {nav_bar_html}
-            <header class="masthead" style="background-image: url('dist/assets/img/post-bg.jpg')">
+            <header class="masthead" style="background-image: url('{BASE_URL}/dist/assets/img/post-bg.jpg')">
                 <div class="container position-relative px-4 px-lg-5">
                     <div class="row gx-4 gx-lg-5 justify-content-center">
                         <div class="col-md-10 col-lg-8 col-xl-7">
@@ -151,23 +149,22 @@ def sync_notion_to_blog():
         with open(os.path.join(SAVE_PATH, file_name), "w", encoding="utf-8") as f:
             f.write(post_page)
 
-    # 🚩 메인 화면 배경 이미지도 'dist/' 에서 가져오기
     index_page = f'''
     <!DOCTYPE html>
     <html lang="ko">
     <head>
-        <title>자동화 퀀트 블로그</title>
+        <title>Hyungbin's LAB</title>
         {head_html}
     </head>
     <body>
         {nav_bar_html}
-        <header class="masthead" style="background-image: url('dist/assets/img/home-bg.jpg')">
+        <header class="masthead" style="background-image: url('{BASE_URL}/dist/assets/img/home-bg.jpg')">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <h1>자동화 퀀트 블로그</h1>
-                            <span class="subheading">노션에 작성하면 15분마다 자동으로 발행됩니다</span>
+                            <h1>Hyungbin's LAB</h1>
+                            <span class="subheading">내봄과 꼬미와 나의 성장을 추구합니다.</span>
                         </div>
                     </div>
                 </div>
