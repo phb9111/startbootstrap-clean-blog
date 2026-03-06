431a93a30110378bf0de0d7ef5c349ee4e5e477e
phb9111 <74388702+phb9111@users.noreply.github.com>
2026-03-06 17:24:13 +0900

    Update new_code.py
    구글 분석도구 추가수정

--- new_code.py
+++ new_code.py
@@ -90,6 +90,9 @@ def sync_notion_to_blog():
 
     head_html = f'''
     <meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <meta name="google-site-verification" content="a0hdbhb-z8TnWjyfn4Ivvf6FTVQ3EVwHftm64LH3k-E" />
+    
     {cache_control}
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
